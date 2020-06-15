0ea777f58c07fb33ab5f46e0ebb5e6cb775301c6
sylar <sylar.huang@all-dream.com>
2020-06-15 18:13:15 +0800

    fix bug in utf8_v8string(do not know why my local node version is wrong........)

--- 2node/src/main.cc
+++ 2node/src/main.cc
@@ -131,6 +131,8 @@ namespace recorder
 
 	Local<String> utf8_v8string(Isolate *isolate,const char *utf8) 
 	{
+		String::NewFromUtf8(isolate, utf8);
+		/*
 #if NODE_VERSION_AT_LEAST(12,0,0)
 		return String::NewFromUtf8(isolate, utf8).ToLocalChecked();
 #elif NODE_VERSION_AT_LEAST(10,0,0)
@@ -138,6 +140,7 @@ namespace recorder
 #else
 		return String::NewFromUtf8(isolate, utf8);
 #endif
+*/
 	}
 
 	bool CheckParamCount(unsigned int nLength, unsigned int nCount) {
